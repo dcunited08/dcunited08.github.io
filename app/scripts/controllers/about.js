209ba0a7340c9f4d7a6a7873373d5bdfc52ac16d
dcook <dcook@hipcricket.com>
2015-02-24 13:19:01 -0500

    initial commit

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name dcunited08githubioApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the dcunited08githubioApp
+ */
+angular.module('dcunited08githubioApp')
+  .controller('AboutCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name dcunited08githubioApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the dcunited08githubioApp
+ */
+angular.module('dcunited08githubioApp')
+  .controller('MainCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
